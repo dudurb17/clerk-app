206436157a6ad6733800963dfc5e6206f27e4a5c
dudurb17 <eduardo.robettibedin@gmail.com>
2024-11-23 18:04:55 -0300

    styles

--- app/(public)/login.tsx
+++ app/(public)/login.tsx
@@ -1,5 +1,6 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+
 
 export default function Login() {
   return (
@@ -8,3 +9,30 @@ export default function Login() {
     </View>
   )
 }
+
+const styles= StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    padding:20
+  },
+  title:{
+    textAlign:'center',
+    fontWeight:'bold',
+    fontSize:28,
+    marginBottom:14
+  },
+  input:{
+    marginVertical:4,
+    height:50,
+    borderWidth:1,
+    borderColor:'#121212',
+    borderRadius:8,
+    padding:10,
+    backgroundColor:'#fff'
+  },
+  button:{
+    margin:8,
+    alignItems:'center'
+  }
+})
