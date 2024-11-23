27ec63d0b23f9bfc2fadfe69dbe3b788cadec6dc
dudurb17 <eduardo.robettibedin@gmail.com>
2024-11-23 19:12:54 -0300

    created logout

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,18 +1,48 @@
-import {Stack} from "expo-router"
+import { useAuth } from "@clerk/clerk-expo";
+import { Feather } from "@expo/vector-icons";
+import { Stack } from "expo-router";
+import { Pressable, TouchableOpacity, View } from "react-native";
 
-export default function StackPage(){
-  return(
-    <Stack 
-    screenOptions={{
-      headerStyle:{
-        backgroundColor:'#121212'
-      },
-      headerTintColor:'#FFF'
-    }}>
-      <Stack.Screen name="home" 
-      options={{headerTitle:'Home'}}/>
-      <Stack.Screen name="profile" 
-      options={{headerTitle:'Profile'}}/>
-    </Stack> 
-  )
+function LogoutButton() {
+  const { signOut } = useAuth();
+  function logout() {
+    signOut();
+  }
+
+  return (
+    <View>
+      <TouchableOpacity onPress={logout}>
+        <Feather name="log-out" size={24} color="#FFF" />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+export default function StackPage() {
+  const { isSignedIn } = useAuth();
+
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#121212",
+        },
+        headerTintColor: "#FFF",
+      }}
+    >
+      <Stack.Screen
+        name="home"
+        options={{ headerTitle: "Home" }}
+        redirect={!isSignedIn}
+      />
+      <Stack.Screen
+        name="profile"
+        options={{
+          headerTitle: "Profile",
+          headerRight: () => <LogoutButton />,
+        }}
+        redirect={!isSignedIn}
+      />
+    </Stack>
+  );
 }
