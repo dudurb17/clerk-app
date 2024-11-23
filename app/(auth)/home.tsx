027b3845936a6d1666d66436708beb74e16d8304
dudurb17 <eduardo.robettibedin@gmail.com>
2024-11-23 18:58:51 -0300

    changed profile

--- app/(auth)/home.tsx
+++ app/(auth)/home.tsx
@@ -1,14 +1,19 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
 
 export default function Home() {
+  const { user } = useUser();
+
   return (
     <View>
       <Text>Home</Text>
+      <Text> Email:{user?.emailAddresses[0].emailAddress}</Text>
+
+      <Link href="/profile">Navegar para profile</Link>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({
-
-})
+const styles = StyleSheet.create({});
--- app/(auth)/profile.tsx
+++ app/(auth)/profile.tsx
@@ -1,10 +1,62 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import { useUser } from "@clerk/clerk-expo";
 
 export default function Profile() {
+  const { user } = useUser();
+
+  const [firstName, setFirstName] = useState(user?.firstName ?? "");
+  const [lastName, setLastName] = useState(user?.lastName ?? "");
+
+  async function handleUpdateProfile() {
+    try {
+      const result = await user?.update({
+        firstName: firstName,
+        lastName: lastName,
+      });
+      console.log('Usuario Atulizado: '+result)
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.container}>
+      <Text style={{textAlign:"center", marginBottom:20, fontSize:20}}>Bem vindo: {user?.fullName && user.fullName}</Text>
+
+      <TextInput
+        value={firstName}
+        onChangeText={setFirstName}
+        placeholder="Digite seu primeiro nome..."
+        style={styles.input}
+      />
+      <TextInput
+        value={lastName}
+        onChangeText={setLastName}
+        placeholder="Digite seu sobrenome"
+        style={styles.input}
+      />
+      <Button
+        title="Atualizar perfil"
+        color="#121212"
+        onPress={handleUpdateProfile}
+      />
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 40,
+  },
+  input: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#121212",
+    borderRadius: 8,
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+});
